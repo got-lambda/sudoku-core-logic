c5ac76c9f97d6afeefc4b69f4634a1bb6a3bb3b5
Jell <jean-louis@jawaninja.com>
2012-12-11 23:50:34 +0100

    More readable get-square.

--- src/sudoku_core_logic/core.clj
+++ src/sudoku_core_logic/core.clj
@@ -2,6 +2,31 @@
   (:refer-clojure :exclude [==])
   (:use clojure.core.logic))
 
+(defn get-squares [cells]
+  (->> cells
+       (partition 3)
+       (partition 3)
+       (apply interleave)
+       (partition 3)
+       (map flatten)))
+
+(defn solve-sudoku [constraints]
+  (first
+   (run 1 [q]
+     (let [sudoku-domain (domain 1 2 3 4 5 6 7 8 9)
+           cells         (repeatedly 81 lvar)
+           rows          (mapv vec (partition 9 cells))
+           columns       (apply map vector rows)
+           squares       (get-squares cells)
+           constraints   (map #(if (zero? %) (lvar) %)
+                              (flatten constraints))]
+       (all
+        (everyg #(infd % sudoku-domain) cells)
+        (== cells constraints)
+        (== q rows)
+        (everyg distinctfd rows)
+        (everyg distinctfd columns)
+        (everyg distinctfd squares))))))
 
 (def super-hard-sudoku
   [[8 0 0  0 0 0  0 0 0]
@@ -15,26 +40,3 @@
    [0 0 1  0 0 0  0 6 8]
    [0 0 8  5 0 0  0 1 0]
    [0 9 0  0 0 0  4 0 0]])
-
-(defn get-squares [rows]
-  (for [i (range 3) j (range 3)]
-    (for [ii (range 3) jj (range 3)]
-      (get-in rows [(+ ii (* 3 i)) (+ jj (* 3 j))]))))
-
-(defn solve-sudoku [constraints]
-  (first
-   (run 1 [q]
-     (let [sudoku-domain (domain 1 2 3 4 5 6 7 8 9)
-           cells         (repeatedly 81 lvar)
-           rows          (mapv vec (partition 9 cells))
-           columns       (apply map vector rows)
-           squares       (get-squares rows)
-           constraints   (map #(if (zero? %) (lvar) %)
-                              (flatten constraints))]
-       (all
-        (everyg #(infd % sudoku-domain) cells)
-        (== cells constraints)
-        (== q rows)
-        (everyg distinctfd rows)
-        (everyg distinctfd columns)
-        (everyg distinctfd squares))))))
