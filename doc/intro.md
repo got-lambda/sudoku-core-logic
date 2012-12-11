0c8222d1645ffee3c4b3b29c9b7b35ee77d833e7
Jell <jean-louis@jawaninja.com>
2012-12-11 18:33:49 +0100

    First commit.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to sudoku-core-logic
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/sudoku_core_logic/core_test.clj
@@ -0,0 +1,7 @@
+(ns sudoku-core-logic.core-test
+  (:use clojure.test
+        sudoku-core-logic.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
